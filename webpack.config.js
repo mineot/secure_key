c84e4e45c2c5809afee4da22a7387c34811135bd
Fabiano Pires <fabianopires@totvs.com.br>
2022-02-11 16:44:23 -0300

    my latest'

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "secure_key.js",
+    library: {
+      name: "secure_key",
+      type: "umd",
+    },
+  },
+};
